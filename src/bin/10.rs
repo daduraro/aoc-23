ca04495f3198e4fd0177c3d6188e0fa30bf817c8
David Duran Rosich <58050048+daduraro@users.noreply.github.com>
2023-12-16 17:03:11 +0100

    Improve day 10

--- src/bin/10.rs
+++ src/bin/10.rs
@@ -1,6 +1,6 @@
 advent_of_code::solution!(10);
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Copy, Clone)]
+#[derive(Debug, PartialEq, Eq, Copy, Clone)]
 enum Direction {
     N = 0,
     S = 1,
@@ -22,12 +22,6 @@ impl std::convert::TryFrom<i32> for Direction {
     }
 }
 
-#[derive(Debug)]
-enum Node {
-    Empty,
-    Connection(Direction, Direction),
-}
-
 fn opposite(dir: Direction) -> Direction {
     match dir {
         Direction::N => Direction::S,
@@ -37,54 +31,86 @@ fn opposite(dir: Direction) -> Direction {
     }
 }
 
+type Grid<T> = ndarray::Array2<T>;
+
+#[derive(Debug, Clone, Copy)]
+enum Node {
+    Empty,
+    Connection(Direction, Direction),
+}
+
 #[derive(Debug)]
 struct Map {
-    nodes: Vec<Node>,
-    width: usize,
-    height: usize,
-    start: usize,
+    grid: Grid<Node>,
+    start: (usize, usize),
 }
 
-fn from_xy(x: isize, y: isize, width: usize) -> Option<usize> {
-    if x < 0 || y < 0 || (x as usize) > width {
-        None
-    } else {
-        Some((x as usize) + (y as usize) * width)
-    }
+trait MapTrait {
+    fn main_loop(&self) -> MapLoop;
 }
 
-fn to_xy(i: usize, width: usize) -> (isize, isize) {
-    ((i % width) as isize, (i / width) as isize)
-}
-
-fn advance(width: usize, height: usize, from: usize, dir: Direction) -> Option<usize> {
-    let (x, y) = to_xy(from, width);
-    let (dx, dy) = match dir {
-        Direction::N => (0, -1),
-        Direction::S => (0, 1),
-        Direction::W => (-1, 0),
-        Direction::E => (1, 0),
-    };
-    let (nx, ny) = (x + dx, y + dy);
-    from_xy(nx, ny, width).and_then(|i| {
-        if i < width * height {
-            Some(i)
-        } else {
-            None
+impl MapTrait for Map {
+    fn main_loop(&self) -> MapLoop {
+        MapLoop {
+            map: self,
+            curr: Some(self.start),
+            next: *neighbours(self, self.start).first().unwrap(),
         }
-    })
-}
-
-fn node_directions(node: &Node) -> Option<(Direction, Direction)> {
-    match node {
-        Node::Empty => None,
-        Node::Connection(a, b) => Some((*a, *b)),
     }
 }
 
-fn neighbours(map: &Map, node: usize) -> Vec<usize> {
-    if let Some((a, b)) = node_directions(&map.nodes[node]){
-        [a, b].iter().filter_map(|dir| advance(map.width, map.height, node, *dir)).collect()
+struct MapLoop<'a> {
+    map: &'a Map,
+    curr: Option<(usize, usize)>,
+    next: (usize, usize),
+}
+
+impl Iterator for MapLoop<'_> {
+    type Item = (usize, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(idx) = self.curr {
+            let new_prev = idx;
+            let new_curr = self.next;
+            let new_next = neighbours(self.map, new_curr).into_iter().find(|x| *x != new_prev).unwrap();
+
+            if new_curr == self.map.start {
+                self.curr = None;
+            }
+            else {
+                self.curr = Some(new_curr);
+            }
+            self.next = new_next;
+
+            Some(new_prev)
+        }
+        else {
+            None
+        }    
+    }
+}
+
+fn advance(pos: (usize, usize), size: (usize, usize), dir: Direction) -> Option<(usize, usize)> {
+    let (i, j) = pos;
+    let (di, dj) = match dir {
+        Direction::N => (-1, 0),
+        Direction::S => (1, 0),
+        Direction::W => (0, -1),
+        Direction::E => (0, 1),
+    };
+    let (ni, nj) = (i as isize + di, j as isize + dj);
+    
+    if ni < 0 || ni >= size.0 as isize || nj < 0 || nj >= size.1 as isize {
+        None
+    }
+    else {
+        Some((ni as usize, nj as usize))
+    }
+}
+
+fn neighbours(map: &Map, node: (usize, usize)) -> Vec<(usize, usize)> {
+    if let Node::Connection(a, b) = map.grid[node] {
+        [a, b].into_iter().filter_map(|dir| advance(node, (map.grid.nrows(), map.grid.ncols()), dir)).collect()
     }
     else {
         vec![]
@@ -102,20 +128,24 @@ fn parse(input: &str) -> Map {
     let mut nodes = Vec::new();
     let mut width = 0;
     let mut height = 0;
+    let mut start = (0, 0);
 
-    for line in input.lines() {
-        for c in line.chars() {
+    for (i, line) in input.lines().enumerate() {
+        for (j, c) in line.chars().enumerate() {
             let v = match c {
-                'S' => None,
-                '.' => Some(Node::Empty),
-                '|' => Some(Node::Connection(Direction::N, Direction::S)),
-                '-' => Some(Node::Connection(Direction::W, Direction::E)),
+                'S' => {
+                    start = (i, j);
+                    Node::Empty
+                },
+                '.' => Node::Empty,
+                '|' => Node::Connection(Direction::N, Direction::S),
+                '-' => Node::Connection(Direction::W, Direction::E),
 
-                'J' => Some(Node::Connection(Direction::N, Direction::W)),
-                'L' => Some(Node::Connection(Direction::N, Direction::E)),
+                'J' => Node::Connection(Direction::N, Direction::W),
+                'L' => Node::Connection(Direction::N, Direction::E),
 
-                '7' => Some(Node::Connection(Direction::S, Direction::W)),
-                'F' => Some(Node::Connection(Direction::S, Direction::E)),
+                '7' => Node::Connection(Direction::S, Direction::W),
+                'F' => Node::Connection(Direction::S, Direction::E),
                 _ => panic!("unexpected character"),
             };
             nodes.push(v);
@@ -128,11 +158,11 @@ fn parse(input: &str) -> Map {
         height += 1;
     }
 
-    let start = nodes.iter().position(|x| x.is_none()).unwrap();
-
+    let mut grid = Grid::from_shape_vec((height, width), nodes).unwrap();
     let conns = (0..4).map(|d| Direction::try_from(d).unwrap()).map(|dir| {
-        if let Some(n) = advance(width, height, start, dir) {
-            (dir, connects(nodes[n].as_ref().unwrap(), opposite(dir)))
+        if let Some(n) = advance(start, (height, width), dir) {
+            let conn = connects(&grid[n], opposite(dir));
+            (dir, conn)
         }
         else {
             (dir, false)
@@ -143,67 +173,42 @@ fn parse(input: &str) -> Map {
         panic!("expected 2 connections to the start node, found {}", conns.len());
     }
 
-    nodes[start] = Some(Node::Connection(conns[0], conns[1]));
+    grid[start] = Node::Connection(conns[0], conns[1]);
 
-    Map { nodes: nodes.into_iter().map(|x| x.unwrap()).collect(), width, height, start }
+    Map { grid, start }
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
     let map = parse(input);
-    let mut current = map.start;
-
-    let mut prev = current;
-    current = neighbours(&map, current).into_iter().next().unwrap();
-
-    let mut n = 1;
-    while current != map.start {
-        let next = neighbours(&map, current).into_iter().find(|x| *x != prev).unwrap();
-        prev = current;
-        current = next;
-        n += 1;
-    }
-
-    Some(n / 2)
+    Some(map.main_loop().count() as u32 / 2)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
     let map = parse(input);
 
-    let is_loop = {
-        let mut visited = Vec::new();
-        visited.resize_with(map.nodes.len(), || false);
+    let mut is_loop = Grid::<bool>::from_elem((map.grid.nrows(), map.grid.ncols()), false);
+    for idx in map.main_loop() {
+        is_loop[idx] = true;
+    }
+    let is_loop = is_loop;
 
-        let mut curr = map.start;
-        let mut next = neighbours(&map, map.start).into_iter().next().unwrap();
-
-        visited[curr] = true;
-        while next != map.start {
-            let prev = curr;
-            curr = next;
-            visited[curr] = true;
-            next = neighbours(&map, curr).into_iter().find(|x| *x != prev).unwrap();
-        }
-        visited
-    };
-
-    let result = (0..map.nodes.len()).filter(|i| {
-        if is_loop[*i] { false } // filter out the main loop
-        else {
-            let a = (i / map.width) * map.width;
-            let parity = (a..*i).filter(|j| {
-                if !is_loop[*j] { false }
-                else if let Node::Connection(a, _) = map.nodes[*j] { // count half top intersection
-                    a == Direction::N
+    let result = (0..map.grid.nrows()).map(|i| {
+        map.grid.row(i).indexed_iter().fold((0u32, 0u32), |(mut acc, mut intersections), (j, n)|{
+            if is_loop[(i, j)] {
+                if let Node::Connection(a, _) = n {
+                    if *a == Direction::N {
+                        intersections += 1;
+                    }
                 }
-                else {
-                    false
-                }
-            }).count();
-            parity % 2 == 1
-        }
-    }).count();
+            }
+            else if intersections % 2 == 1 {
+                acc += 1;
+            }
+            (acc, intersections)
+        }).0
+    }).sum();
 
-    Some(result as u32)
+    Some(result)
 }
 
 #[cfg(test)]
@@ -242,6 +247,5 @@ mod tests {
         let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 6));
         assert_eq!(result, Some(10));
     }
-
 
 }
